6ea7aebabe8f939e5154936360fa3331e1ab90fe
jbm <jbm@netcompany.com>
2019-11-14 17:33:19 +0100

    *** Added airports and Cleanup ***

---
+++ SolutionAccelerator/Migrations/201911141630553_AirportModel.cs
@@ -0,0 +1,39 @@
+namespace SolutionAccelerator.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AirportModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Airport",
+                c => new
+                    {
+                        AirportId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Availability = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.AirportId);
+            
+            DropTable("dbo.Transaction");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Transaction",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        PacketId = c.Int(nullable: false),
+                        Routes = c.String(),
+                        Date = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            DropTable("dbo.Airport");
+        }
+    }
+}
